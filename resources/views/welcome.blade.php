6ead818e2cc2778ef59555c6ed7651827b8d7c79
Omar Ajmi <devdevooops@gmail.com>
2018-10-23 00:49:46 +0100

    fix welcome page slider.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,47 +25,47 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="cover_nav">
-                    <ul class="cover_dots">
-                        <li class="active" data="0"><span>1</span></li>
-                        @for ($i = 1; $i <= $events->first()->sliders->count(); $i++)
-                            <li data="{{ $i }}"><span>{{ $i+1 }}</span></li>
-                        @endfor
-                    </ul>
-                </div>
                 @endforeach
-            @else
-                <div class="cover_slider owl-carousel owl-theme">
-                    <div class="cover_item" style="background: url('img/bg/background01.jpg');">
-                        <div class="slider_content">
-                            <div class="slider-content-inner">
-                                <div class="container">
-                                    <div class="slider-content-center">
-                                        <h2 class="cover-title">
-                                            Prepare yourself for the
-                                        </h2>
-                                        <strong class="cover-xl-text">conference</strong>
-                                        <p class="cover-date">
-                                            12-14 February 2018 - Los Angeles, CA.
-                                        </p>
-                                        <a href="#" class=" btn btn-primary btn-rounded">
-                                            Buy Tickets Now
-                                        </a>
-                                    </div>
+            </div>
+            <div class="cover_nav">
+                <ul class="cover_dots">
+                    <li class="active" data="0"><span>1</span></li>
+                    @for ($i = 1; $i <= $events->first()->sliders->count(); $i++)
+                        <li data="{{ $i }}"><span>{{ $i+1 }}</span></li>
+                    @endfor
+                </ul>
+            </div>
+        @else
+            <div class="cover_slider owl-carousel owl-theme">
+                <div class="cover_item" style="background: url('img/bg/background01.jpg');">
+                    <div class="slider_content">
+                        <div class="slider-content-inner">
+                            <div class="container">
+                                <div class="slider-content-center">
+                                    <h2 class="cover-title">
+                                        Prepare yourself for the
+                                    </h2>
+                                    <strong class="cover-xl-text">conference</strong>
+                                    <p class="cover-date">
+                                        12-14 February 2018 - Los Angeles, CA.
+                                    </p>
+                                    <a href="#" class=" btn btn-primary btn-rounded">
+                                        Buy Tickets Now
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="cover_nav">
-                    <ul class="cover_dots">
-                        <li class="active" data="0"><span>1</span></li>
-                        <li data="1"><span>2</span></li>
-                        <li data="2"><span>3</span></li>
-                    </ul>
-                </div>
-            @endif
+            </div>
+            <div class="cover_nav">
+                <ul class="cover_dots">
+                    <li class="active" data="0"><span>1</span></li>
+                    <li data="1"><span>2</span></li>
+                    <li data="2"><span>3</span></li>
+                </ul>
+            </div>
+        @endif
     </section>
     <!--cover section slider end -->
     
