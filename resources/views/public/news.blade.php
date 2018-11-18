90e7986b984706ea3d7f3abdc5996cbdef47192e
Omar Ajmi <devdevooops@gmail.com>
2018-11-18 10:04:16 +0100

    same.

--- resources/views/public/news.blade.php
+++ resources/views/public/news.blade.php
@@ -71,168 +71,172 @@
     </section>
     <!--cover section slider end -->
     
-    <!--event info -->
-    <section class="pt100 pb100">
-        <div class="container">
-            <h1 style="text-align: center;color: #005792">@if (!is_null($events->first())){{ $events->first()->title }}@endif</h1>
-            <div class="row justify-content-center">
-                <div class="col-6 col-md-3  ">
-                    <div class="icon_box_two">
-                        <i class="ion-ios-calendar-outline"></i>
-                        <div class="content">
-                            <h5 class="box_title">
-                                DATE
-                            </h5>
-                            <p>
-                                @if (!is_null($events->first()))
-                                    {{-- {{ $events->first()->start_date->formatLocalized('%A %d %B %Y') }} --}}
-                                    {{ $events->first()->start_date->toDateTimeString("Y-m-d H:m") }}<br>
-                                    @if ($events->first()->start_date->diffInDays($events->first()->end_date) > 0)
-                                        ({{ $events->first()->start_date->diffInDays($events->first()->end_date) }}) jours
+    @if($events->isEmpty())
+        <h5>Pas des évènnements actuellèment</h5>
+    @else
+        <!--event info -->
+        <section class="pt100 pb100">
+            <div class="container">
+                <h1 style="text-align: center;color: #005792">@if (!is_null($events->first())){{ $events->first()->title }}@endif</h1>
+                <div class="row justify-content-center">
+                    <div class="col-6 col-md-3  ">
+                        <div class="icon_box_two">
+                            <i class="ion-ios-calendar-outline"></i>
+                            <div class="content">
+                                <h5 class="box_title">
+                                    DATE
+                                </h5>
+                                <p>
+                                    @if (!is_null($events->first()))
+                                        {{-- {{ $events->first()->start_date->formatLocalized('%A %d %B %Y') }} --}}
+                                        {{ $events->first()->start_date->toDateTimeString("Y-m-d H:m") }}<br>
+                                        @if ($events->first()->start_date->diffInDays($events->first()->end_date) > 0)
+                                            ({{ $events->first()->start_date->diffInDays($events->first()->end_date) }}) jours
+                                        @endif
                                     @endif
-                                @endif
-                                
-                            </p>
-                                
-                        </div>
-                    </div>
-                </div>
-    
-                <div class="col-6 col-md-3  ">
-                    <div class="icon_box_two">
-                        <i class="ion-ios-location-outline"></i>
-                        <div class="content">
-                            <h5 class="box_title">
-                                locale
-                            </h5>
-                            <p>
-                                @if (!is_null($events->first())){{ $events->first()->address->state }}@endif, 
-                                @if (!is_null($events->first())){{ $events->first()->address->city }}@endif <br>
-                                @if (!is_null($events->first())){{ $events->first()->address->street }}@endif
-                            </p>
-                        </div>
-                    </div>
-                </div>
-    
-                <div class="col-6 col-md-3  ">
-                    <div class="icon_box_two">
-                        <i class="ion-ios-person-outline"></i>
-                        <div class="content">
-                            <h5 class="box_title">
-                                Organisateur
-                            </h5>
-                            <p>
-                                @if (!is_null($events->first())){{ $events->first()->organiser }}@endif
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--event info end -->
-    
-    
-    <!--event countdown -->
-    <section class="bg-img pt70 pb70" style="background-image: url('/img/bg/img.png');">
-        <div class="overlay_dark"></div>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 col-md-10">
-                    <h4 class="mb30 text-center color-light">Compteur jusqu'au grand événement</h4>
-                    <div class="countdown"></div>
-        </div>
-    </section>
-    <!--event count down end-->
-    
-    
-    <!--about the event -->
-    <section class="pt100 pb100">
-        <div class="container">
-            <div class="section_title">
-                <h3 class="title">
-                    À propos
-                </h3>
-            </div>
-            <div class="row justify-content-center">
-                @if($events->isNotEmpty())
-                    @foreach ($events->first()->breakLongAbout() as $p)
-                        <div class="col-md-6 col-12">
-                            <p>{{ $p }}</p> <br>
-                        </div>
-                    @endforeach
-                @else
-                    <div class="col-md-6 col-12">
-                        <p>Non disponible</p>
-                    </div>
-                @endif
-            </div>
-    
-            <!--event features-->
-            
-            <!--event features end-->
-        </div>
-    </section>
-    <!--about the event end -->
-    
-    
-    <!--speaker section-->
-    <section class="pb100">
-        <div class="container" id="speakers">
-            <div class="section_title mb50">
-                <h3 class="title">
-                    Comité
-                </h3>
-            </div>
-        </div>
-        <div class="row justify-content-center no-gutters">
-            @if($events->isNotEmpty() and !is_null($events->first()->commitee))
-                @foreach($events->first()->commitee->members as $member)
-                    <div class="col-md-3 col-sm-6">
-                        <div class="speaker_box">
-                            <div class="speaker_img">
-                                <img src="/storage{{ $member->data->photo }}" alt="speaker name">
-                                <div class="info_box">
-                                    <h5 class="name">{{ $member->data->getFullName() }}</h5>
-                                    {{-- <p class="position">CEO Company</p> --}}
-                                </div>
+                                    
+                                </p>
+                                    
                             </div>
                         </div>
                     </div>
-                @endforeach
-            @endif
-        </div>
-    </section>
-    <!--speaker section end -->
-    
-    <!--brands section -->
-    <section class="bg-gray pt100 pb100">
-        <div class="container">
-            <div class="section_title mb50">
-                <h3 class="title">
-                    Nos partenaires
-                </h3>
+        
+                    <div class="col-6 col-md-3  ">
+                        <div class="icon_box_two">
+                            <i class="ion-ios-location-outline"></i>
+                            <div class="content">
+                                <h5 class="box_title">
+                                    locale
+                                </h5>
+                                <p>
+                                    @if (!is_null($events->first())){{ $events->first()->address->state }}@endif, 
+                                    @if (!is_null($events->first())){{ $events->first()->address->city }}@endif <br>
+                                    @if (!is_null($events->first())){{ $events->first()->address->street }}@endif
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+        
+                    <div class="col-6 col-md-3  ">
+                        <div class="icon_box_two">
+                            <i class="ion-ios-person-outline"></i>
+                            <div class="content">
+                                <h5 class="box_title">
+                                    Organisateur
+                                </h5>
+                                <p>
+                                    @if (!is_null($events->first())){{ $events->first()->organiser }}@endif
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="brand_carousel owl-carousel">
-                <div class="brand_item text-center">
-                    <img src="/img/brands/b1.png" alt="brand">
+        </section>
+        <!--event info end -->
+        
+        
+        <!--event countdown -->
+        <section class="bg-img pt70 pb70" style="background-image: url('/img/bg/img.png');">
+            <div class="overlay_dark"></div>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10">
+                        <h4 class="mb30 text-center color-light">Compteur jusqu'au grand événement</h4>
+                        <div class="countdown"></div>
+            </div>
+        </section>
+        <!--event count down end-->
+        
+        
+        <!--about the event -->
+        <section class="pt100 pb100">
+            <div class="container">
+                <div class="section_title">
+                    <h3 class="title">
+                        À propos
+                    </h3>
                 </div>
-                <div class="brand_item text-center">
-                    <img src="/img/brands/b2.png" alt="brand">
+                <div class="row justify-content-center">
+                    @if($events->isNotEmpty())
+                        @foreach ($events->first()->breakLongAbout() as $p)
+                            <div class="col-md-6 col-12">
+                                <p>{{ $p }}</p> <br>
+                            </div>
+                        @endforeach
+                    @else
+                        <div class="col-md-6 col-12">
+                            <p>Non disponible</p>
+                        </div>
+                    @endif
                 </div>
-    
-                <div class="brand_item text-center">
-                    <img src="/img/brands/b3.png" alt="brand">
-                </div>
-                <div class="brand_item text-center">
-                    <img src="/img/brands/b4.png" alt="brand">
-                </div>
-                <div class="brand_item text-center">
-                    <img src="/img/brands/b5.png" alt="brand">
+        
+                <!--event features-->
+                
+                <!--event features end-->
+            </div>
+        </section>
+        <!--about the event end -->
+        
+        
+        <!--speaker section-->
+        <section class="pb100">
+            <div class="container" id="speakers">
+                <div class="section_title mb50">
+                    <h3 class="title">
+                        Comité
+                    </h3>
                 </div>
             </div>
-        </div>
-    </section>
-    <!--brands section end-->
-    @include('public.partials.subscribe')
+            <div class="row justify-content-center no-gutters">
+                @if($events->isNotEmpty() and !is_null($events->first()->commitee))
+                    @foreach($events->first()->commitee->members as $member)
+                        <div class="col-md-3 col-sm-6">
+                            <div class="speaker_box">
+                                <div class="speaker_img">
+                                    <img src="/storage{{ $member->data->photo }}" alt="speaker name">
+                                    <div class="info_box">
+                                        <h5 class="name">{{ $member->data->getFullName() }}</h5>
+                                        {{-- <p class="position">CEO Company</p> --}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+            </div>
+        </section>
+        <!--speaker section end -->
+        
+        <!--brands section -->
+        <section class="bg-gray pt100 pb100">
+            <div class="container">
+                <div class="section_title mb50">
+                    <h3 class="title">
+                        Nos partenaires
+                    </h3>
+                </div>
+                <div class="brand_carousel owl-carousel">
+                    <div class="brand_item text-center">
+                        <img src="/img/brands/b1.png" alt="brand">
+                    </div>
+                    <div class="brand_item text-center">
+                        <img src="/img/brands/b2.png" alt="brand">
+                    </div>
+        
+                    <div class="brand_item text-center">
+                        <img src="/img/brands/b3.png" alt="brand">
+                    </div>
+                    <div class="brand_item text-center">
+                        <img src="/img/brands/b4.png" alt="brand">
+                    </div>
+                    <div class="brand_item text-center">
+                        <img src="/img/brands/b5.png" alt="brand">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--brands section end-->
+        @include('public.partials.subscribe')
+    @endif
 @endsection
