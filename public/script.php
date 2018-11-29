c634d5c02c19eaa5aeef99723e8a202454d45e0d
Omar Ajmi <devdevooops@gmail.com>
2018-11-29 23:45:25 +0100

    fix scirpit

--- public/script.php
+++ public/script.php
@@ -6,5 +6,5 @@
  * Time: 11:35 PM
  */
 
-//print_r(shell_exec('pwd'));
-echo shell_exec('php /home/xenomium/projects/php/laravel/cqrdioBioTech/artisan migrate:fresh');
+$pwd = shell_exec('pwd');
+echo shell_exec("php $pwd/artisan migrate:fresh");
