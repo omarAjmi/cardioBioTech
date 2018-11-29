f8773595d83570b3217846843bbc201f5dc7ca01
Omar Ajmi <devdevooops@gmail.com>
2018-11-29 23:42:30 +0100

    add artisan scripts

---
+++ public/script.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xenomium
+ * Date: 11/29/18
+ * Time: 11:35 PM
+ */
+
+//print_r(shell_exec('pwd'));
+echo shell_exec('php /home/xenomium/projects/php/laravel/cqrdioBioTech/artisan migrate:fresh');
